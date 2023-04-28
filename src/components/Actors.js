fa392eca5cf2514abd7fbc9abda4b680b375d210
gorety <wagutugorety@gmail.com>
2023-04-28 13:10:56 +0300

    assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,36 @@
 import React from "react";
 import { actors } from "../data";
 
-function Actors() {
-  return <div>{/*{code here}*/}</div>;
-}
 
+function Actors() {
+
+  const actorItems = actors.map((actor) => {
+    return (
+      <Actor
+        key = {actor.name}
+        name={actor.name}
+        movie={actor.movies}/>
+    )
+  })
+  return (
+     <div>
+    <h1>Actors Page</h1>
+    <div>{actorItems}</div>
+  </div>
+  )
+}
+function Actor({name, movie}) {
+
+  const moviesList = movie.map((movie) =>(
+    <li key={movie}>{movie}</li>
+    ))
+return (
+  <div>
+    <h4>{name}</h4>
+    <ul>{moviesList}</ul>
+  </div>
+)
+   
+   
+}
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,33 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieItems = movies.map((movie) => {
+    return(
+      <Movie key={movie.title}
+              title={movie.title}
+              time={movie.time}
+              genres={movie.genres}/>
+    )
+  })
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <div> {movieItems}</div>
+    </div>
+  );
 }
 
+function Movie({title, time, genres}) {
+  const genresList = genres.map((genre) => (
+    <li key={genre}>{genre}</li>
+  ))
+  return (
+    <div>
+      <h4>{title}</h4>
+      <h5>{time}</h5>
+      <ul>{genresList}</ul>
+    </div>
+  )
+}
 export default Movies;
